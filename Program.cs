c15726095ecb9b42d102c56bc2aecd28d27b57aa
Ali Kheyrollahi <aliostad@gmail.com>
2022-04-13 22:18:05 +0100

    first init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace TraceSwitchDoesNotWork
+{
+  // exactly as in docs:
+  // https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.traceswitch?view=net-6.0
+  class Program
+  {
+ 
+    private static TraceSwitch appSwitch = new TraceSwitch("mySwitch",
+      "Switch in config file");
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine(ConfigurationManager.AppSettings["Something"]); // prints hello, correct
+      Console.WriteLine(appSwitch.Level.ToString()); // prints Off, WRONG! Off means 0.
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TraceSwitchDoesNotWork
-A minimal repo on .NETT 6.0 to show that unlike what the docs say 
+A minimal repo on .NET 6.0 to show that unlike what the [docs](https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.traceswitch?view=net-6.0) say.
+
+The code has been taken verbatim from the snippets in the docs above. But simply does not work.
